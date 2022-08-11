15b098a35f698ce454fbb109d363a97bfcff3dce
LucaPalminteri <lucapalminteri02@gmail.com>
2022-08-10 23:42:35 -0300

    fix data shown problems

--- connectToDatabaseXampp.js
+++ connectToDatabaseXampp.js
@@ -1,17 +1,29 @@
 const mysql = require('mysql')
 var express = require('express');
+const path = require('path')
+const fs = require('fs');
 var app = express();
 
 // Express
 
-const cors = require("cors");
-app.use(cors());
+app.get('/', function (req, res,next) {
+    res.writeHead(200, {'content-type': 'text/html'})
+    //res.send("./index.html");
+    fs.createReadStream('index.html').pipe(res);
+    // res.sendFile('main.js', {root: path.join(__dirname)}, (err) => {
+    //     if (err) {
+    //         next(err);
+    //     } else {
+    //         console.log('File Sent:', 'main.js');
+    //     }
+    // });
+ })
 
 var server = app.listen(3000, function () {
    var host = server.address().address
    var port = server.address().port
 
-   console.log("Example app listening at http://%s:%s", host, port)
+   console.log("Example app listening at http://", host, port)
 })
 
 
--- main.js
+++ main.js
@@ -1,15 +1,5 @@
 const btnConsultar = document.getElementById('btn-consultar') //as HTMLButtonElement;
 
 btnConsultar.addEventListener('click', ()=>{
-    fetch('http://localhost:3000/usuarios'
-    , {
-        mode: 'no-cors',
-        method: 'get',
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify()}
-    )
-        .then(data => console.log(data))
-        .catch(err => console.log(err))
+    console.log("click");
 });
